f0a07f3654e9fed98dbfafb1407ecf5d39d70aee
Sawan.Patwari <sawan.patwari@yahoo.in>
2018-05-06 16:38:02 +0530

    Local Git changes - TestProgram.java

--- SamplePrograms/src/myDefault/TestProgram.java
+++ SamplePrograms/src/myDefault/TestProgram.java
@@ -4,7 +4,8 @@ public class TestProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test Successful. Workspace created.");
+		System.out.println("Test Successful. Workspace created. "
+				+ "Change in local git, but not in Remote git.");
 	}
 
 }
