fb6264cb9a568d084e912303a7458ba92f33498f
Sawan.Patwari <sawan.patwari@yahoo.in>
2018-05-06 16:21:56 +0530

    Local GIT Commit - TestProgram.java

--- SamplePrograms/src/myDefault/TestProgram.java
+++ SamplePrograms/src/myDefault/TestProgram.java
@@ -4,7 +4,9 @@ public class TestProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test Successful. Workspace created.");
+		System.out.println("Test Successful. Workspace created. "
+				+ "Made change only in the local git and "
+				+ "then reverted in the local Eclipse workspace.");
 	}
 
 }
