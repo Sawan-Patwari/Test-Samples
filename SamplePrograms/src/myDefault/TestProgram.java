01f2988dc3126294e2d6c60f542e873a7e721f13
Sawan.Patwari <sawan.patwari@yahoo.in>
2018-05-06 16:24:08 +0530

    Reverted the changes - TestProgram.java

--- SamplePrograms/src/myDefault/TestProgram.java
+++ SamplePrograms/src/myDefault/TestProgram.java
@@ -4,9 +4,7 @@ public class TestProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test Successful. Workspace created. "
-				+ "Made change only in the local git and "
-				+ "then reverted in the local Eclipse workspace.");
+		System.out.println("Test Successful. Workspace created.");
 	}
 
 }
