e6dd81392d0c046ec72a7db5e2edbd6b5e125a4d
Sawan.Patwari <sawan.patwari@yahoo.in>
2018-05-15 01:15:50 +0530

    No Comments.

--- SamplePrograms/src/test/threads/ExecutorServiceProg.java
+++ SamplePrograms/src/test/threads/ExecutorServiceProg.java
@@ -1,21 +1,71 @@
 package test.threads;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * 
+ * @author Sawan.Patwari
+ *
+ */
 public class ExecutorServiceProg {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		doSampleExecuteTest();
-		doSampleSubmitTest();
+		int FIVE_THREADS = 5;
+		int THREE_THREADS = 3;
+		long FIVE_SECONDS = 5_000;
+		
+		//It would be better if we make the below calls through separate programs.
+		ExecutorService service = Executors.newSingleThreadExecutor();
+		doSampleExecuteTest(service);//we are shutting down the service. 
+		//Need new service from the factory.
+		Thread.sleep(FIVE_SECONDS);
+		service = Executors.newSingleThreadExecutor();
+		doSampleSubmitTest(service);
+		
+		Thread.sleep(FIVE_SECONDS);
+		
+		//It would be better if we make the below calls through separate programs.
+		service = Executors.newCachedThreadPool();
+		doSampleExecuteTest(service);		
+		Thread.sleep(FIVE_SECONDS);
+		service = Executors.newCachedThreadPool();
+		doSampleSubmitTest(service);
+		
+		Thread.sleep(FIVE_SECONDS);
+		
+		//It would be better if we make the below calls through separate programs.
+		service = Executors.newFixedThreadPool(FIVE_THREADS);
+		doSampleExecuteTest(service);
+		Thread.sleep(FIVE_SECONDS);
+		service = Executors.newFixedThreadPool(FIVE_THREADS);
+		doSampleSubmitTest(service);
+		
+		Thread.sleep(FIVE_SECONDS);
+		
+		//It would be better if we make the below calls through separate programs.
+		ScheduledExecutorService scheduledService = Executors.newSingleThreadScheduledExecutor();
+		doSampleScheduleTest(scheduledService);
+		Thread.sleep(FIVE_SECONDS);
+		scheduledService = Executors.newScheduledThreadPool(THREE_THREADS);
+		doSampleScheduleTest(scheduledService);
+		
+		/* Basic Test Pattern:
+		ExecutorService service = Executors.newSingleThreadExecutor();
+		
+		doSampleExecuteTest(service);
+		doSampleSubmitTest(service);
+		doSampleScheduleTest(service);*/
 	}
 
-	static void doSampleExecuteTest() {
-		ExecutorService service = null;
+	static void doSampleExecuteTest(ExecutorService service) {
+		
 		try {
 			// SingleThreadExecutor: no guarantee in the order of execution of tasks when
 			// more threads are created. Order of execution is preserved for small number
@@ -56,11 +106,9 @@ public class ExecutorServiceProg {
 		 */
 	}
 	
-	static void doSampleSubmitTest() {
-		ExecutorService service = null;
+	static void doSampleSubmitTest(ExecutorService service) {
+		
 		try {
-			//'Executors' - A Factory pattern.
-			service = Executors.newSingleThreadExecutor();
 			
 			//This is a Runnable Task submission, as there exists no return.
 			Future<?> futureOfTask1 = service.submit(() -> System.out.println("Task-1"));
@@ -106,5 +154,61 @@ public class ExecutorServiceProg {
 				System.out.println("At least one task is still running");
 		}
 	}
+	
+	@SuppressWarnings("unused")
+	static void doSampleScheduleTest(ScheduledExecutorService service) {
 
+		System.out.println("[doSampleScheduleTest()]: I started!");
+
+		Runnable runnableTask1 = () -> System.out.println("Runnable Task-1 Done.");
+		Runnable runnableTask2 = () -> {
+			String taskNumber = "Task-2." + TaskNumberIncrementer.getTask2DecimalIncrementedValue();
+			System.out.println("Recurring Runnable " + taskNumber + " Done.");
+		};
+		Runnable runnableTask3 = () -> {
+			String taskNumber = "Task-3." + TaskNumberIncrementer.getTask3DecimalIncrementedValue();
+			System.out.println("Recurring Runnable " + taskNumber + " Done.");
+		};
+		Callable<String> callableTask = () -> "Callable Task Done.";
+
+		Future<?> runnableResult1 = service.schedule(runnableTask1, 10, TimeUnit.SECONDS);
+		Future<String> callableResult = service.schedule(callableTask, 1, TimeUnit.SECONDS);
+
+		Future<?> runnableResult2 = service.scheduleAtFixedRate(runnableTask2, 1, 1, TimeUnit.MINUTES);
+
+		Future<?> runnableResult3 = service.scheduleWithFixedDelay(runnableTask3, 1, 2, TimeUnit.MINUTES);
+
+		System.out.println("[doSampleScheduleTest()]: I am done with Runnable tasks!");
+
+		try {
+			System.out.println("[doSampleScheduleTest()]: I have time to wait for Callable " + "task output. "
+					+ "The output is:" + callableResult.get());
+		} catch (InterruptedException | ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("[doSampleScheduleTest()]: I am exiting now.");
+	}
+
+}
+
+/**
+ * This class acts as a state maintainer for the lambda Runnable/Callable code.
+ * This class sample is to know the previous value before getting the next task version number
+ * inside the lambda Runnable code.
+ * 
+ * @author Sawan.Patwari
+ *
+ */
+class TaskNumberIncrementer {
+	private static int task2Incrementer = -1;
+	private static int task3Incrementer = -1;
+
+	public static int getTask2DecimalIncrementedValue() {
+		return ++task2Incrementer;
+	}
+
+	public static int getTask3DecimalIncrementedValue() {
+		return ++task3Incrementer;
+	}
 }
--- SamplePrograms/src/test/threads/ThreadPollingSampleProg.java
+++ SamplePrograms/src/test/threads/ThreadPollingSampleProg.java
@@ -5,6 +5,11 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+/**
+ * 
+ * @author Sawan.Patwari
+ *
+ */
 public class ThreadPollingSampleProg {
 
 	private static boolean wait;
