593df8966b21f0c933e9a01fc8780c3712446457
Sawan.Patwari <sawan.patwari@yahoo.in>
2018-06-22 16:19:38 +0530

    No Comments

--- SamplePrograms/src/defaultPackage/TestProjectSetupProg.java
+++ SamplePrograms/src/defaultPackage/TestProjectSetupProg.java
@@ -1,6 +1,6 @@
-package myDefault;
+package defaultPackage;
 
-public class TestProgram {
+public class TestProjectSetupProg {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
