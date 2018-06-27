5d1d9f0fc69c2002835b2f4576d0e34dc1776aa0
Sawan.Patwari <sawan.patwari@yahoo.in>
2018-06-27 05:54:11 +0530

    Spring Boot 2.x setup sample-1.

---
+++ SpringRestSample-1/src/main/java/com/example/SpringRestSample1/SpringRestSample1Application.java
@@ -0,0 +1,12 @@
+package com.example.SpringRestSample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestSample1Application.class, args);
+	}
+}
