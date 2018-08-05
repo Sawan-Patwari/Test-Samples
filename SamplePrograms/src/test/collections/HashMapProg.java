f41c9bdf1ad8745c63b18c5984b31a741c6f00ee
Sawan.Patwari <sawan.patwari@yahoo.in>
2018-08-06 02:30:03 +0530

    No Comments.

--- SamplePrograms/src/test/collections/HashMapProg.java
+++ SamplePrograms/src/test/collections/HashMapProg.java
@@ -1,7 +1,9 @@
 package test.collections;
 
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
@@ -17,7 +19,7 @@ public class HashMapProg {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		test5();
+		test6();
 	}
 
 	public static void test1() {
@@ -137,6 +139,44 @@ public class HashMapProg {
 		System.out.println(map.size());
 
 	}
+	
+	public static void test6() {
+		Employee4 x = new Employee4();
+		x.a = 10;
+		x.b = 20;
+
+		Employee4 x1 = new Employee4();
+		x1.a = 10;
+		x1.b = 20;
+		
+		Employee4 x2 = new Employee4();
+		x2.a = 05;
+		x2.b = 35;
+
+		Employee4 x3 = new Employee4();
+		x3.a = 95;
+		x3.b = 05;
+		
+		Employee4 x4 = new Employee4();
+		x4.a = 75;
+		x4.b = 10;
+		
+		//Cannot sort the HashMap on its own.
+		Map<Employee4, Employee4> map = new HashMap<>();
+		map.put(x, x);
+		map.put(x1, x1);
+		map.put(x2, x2);
+		map.put(x3, x3);
+		map.put(x4, x4);
+		
+		Map<Employee4, Employee4> sortMap = new TreeMap<>(Employee4.Comparators.sortAscendingByAFieldWithDupeEntries);
+		sortMap.putAll(map);
+		System.out.println("Size:"+map.size());
+		sortMap.forEach((m, n) -> {
+			System.out.println(m.a + " " +n.b);
+		});
+		
+	}
 
 	class Employee {
 		int a;
@@ -211,6 +251,51 @@ public class HashMapProg {
 			return HashCodeBuilder.reflectionHashCode(this);
 		}
 
+	}
+
+}
+
+class Employee4 {
+	int a;
+	int b;
+
+	static class Comparators {
+
+		static Comparator<Employee4> sortAscendingByAFieldWithDupeEntries = (e1, e2) -> {
+			if (e1.a <= e2.a) {
+				return -1;
+			} else {
+				return 1;
+			}
+		};
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+
+		// simple one line code:
+		// return EqualsBuilder.reflectionEquals(this, (Employee3)obj);
+
+		// better improved code:
+		Employee4 other = (Employee4) obj;
+		
+		if(this == other) {
+			return true;
+		} else {
+		
+			if (this.hashCode() == other.hashCode()) {
+				return EqualsBuilder.reflectionEquals(this, other);
+			} else {
+				return false;
+			}
+		}
+		
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
 	}
 
 }
