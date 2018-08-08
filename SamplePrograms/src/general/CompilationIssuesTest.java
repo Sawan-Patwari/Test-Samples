e0997d21e6908b890919c6b6b63c017a33991133
Sawan.Patwari <sawan.patwari@yahoo.in>
2018-08-08 19:02:54 +0530

    No Comments.

---
+++ SamplePrograms/src/general/CompilationIssuesTest.java
@@ -0,0 +1,39 @@
+package general;
+
+/**
+ * 
+ * @author Sawan.Patwari
+ *
+ */
+public class CompilationIssuesTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
+
+class Test1{
+		static class Parent {
+			public static void test() {
+				System.out.println("Calling from Parent: test()");
+			}
+			
+			public void test1() {
+				System.out.println("Calling from Parent: test1()");
+			}
+		}
+		
+		class Child extends Parent {
+			
+			public void test() {
+				System.out.println("Calling from Child: test()");
+			}
+			
+			public static void test1() {
+				System.out.println("Calling from Child: test1()");
+			}
+			
+		}
+}
